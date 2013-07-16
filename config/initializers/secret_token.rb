6d489fb3a5b9888dce79276d5bd8116265d1e6b0
John Shaughnessy <johnfshaughnessy@gmail.com>
2013-07-16 03:31:14 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instacake::Application.config.secret_token = 'fa0c4a3c1c996181bcba1376f6cfbb7c672288de4ae79532c2503731d716f9248f58182f954f2dd274e08a2ac31d5377acc748c5a4d00c1473b1147f81483519'
