e51cedf501efd87cc4e2615bf356e27c09b91b16
John Shaughnessy <johnfshaughnessy@gmail.com>
2013-07-17 16:53:28 -0700

    i'm actually a dummy

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,5 @@
 class Project < ActiveRecord::Base
-  attr_accessible :insta_thumb_uri, :insta_vid_uri, 
-  :local_thumb_uri, :local_vid_uri, :title
+  attr_accessible :title
 
   has_many :assignments
   has_many :users, through: :assignments
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,7 @@
 class Video < ActiveRecord::Base
   attr_accessible :created_at, :insta_id
-
+  
+  has_many :vp_assignments
   has_many :projects, through: :vp_assignments
   belongs_to :user
 end
