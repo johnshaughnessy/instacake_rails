328b77462544422161b2a68fa6ffca594f32fad1
John Shaughnessy <johnfshaughnessy@gmail.com>
2013-08-05 13:50:53 -0700

    vidoe changes

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,5 @@
 class Video < ActiveRecord::Base
-  attr_accessible :created_at, :uid
+  attr_accessible :created_at, :uid, :thumbnail, :vid_uri
   
   has_many :vp_assignments
   has_many :projects, through: :vp_assignments
